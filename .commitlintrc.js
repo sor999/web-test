2174738892cb851173f0ef571431a6362d29e2c8
hyeon-aa <ahhyun1120@naver.com>
2026-01-12 22:55:39 +0900

    init: husky 설정

---
+++ .commitlintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'chore',
+        'design',
+        'fix',
+        'test',
+        'deploy',
+        'refactor',
+        'docs',
+        'init',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [0],
+  },
+}
